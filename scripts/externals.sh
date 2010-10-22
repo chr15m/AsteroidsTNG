d444c32e9b15475415039bdf88282dcb174b1f3a
Chris McCormick <chris@mccormick.cx>
2010-10-22 22:10:22 +0800

    Shuffling files around to start server.

--- scripts/externals.sh
+++ scripts/externals.sh
@@ -3,6 +3,4 @@
 rm -rf explorercanvas
 svn export http://explorercanvas.googlecode.com/svn/trunk/ explorercanvas
 rm -rf jsgamesoup
-bzr export jsgamesoup -r74 http://mccormick.cx/dev/jsgamesoup
-rm js/mt.js
-wget http://homepage2.nifty.com/magicant/sjavascript/mt.js -O js/mt.js
+bzr export static/jsGameSoup http://mccormick.cx/dev/jsgamesoup
